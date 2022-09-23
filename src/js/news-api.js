b6ecf1405c5e290492bdc1a3942231def2363213
Ivan Shapovalov <Shapovalov.bizz@gmail.com>
2022-09-23 23:34:39 +0300

    Update

--- src/js/news-api.js
+++ src/js/news-api.js
@@ -10,21 +10,16 @@ export default class NewsApiService {
     const URL = 'https://pixabay.com/api/';
     const KEY = '30097880-73ac2834789f98742941535c7';
     let searchParams = `per_page=40&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}`;
-    // return fetch(`${URL}?key=${KEY}&q=${this.searchQuery}&${searchParams}`)
-    //   .then(result => result.json())
-    //   .then(data => {
-    //     this.incrementPage();
-    //     return data.hits;
-    //   });
- const response = await axios(
-   `${URL}?key=${KEY}&q=${this.searchQuery}&${searchParams}`
- );
+    const response = await axios(
+      `${URL}?key=${KEY}&q=${this.searchQuery}&${searchParams}`
+    );
 
-     if (response.status === 200) {
-       return response.data;
-     } else {
-       throw new Error(response.statusText);
-     }
+    if (response.status === 200) {
+      console.log(response.data);
+      return response.data.hits;
+    } else {
+      throw new Error(response.statusText);
+    }
   }
 
   incrementPage() {
